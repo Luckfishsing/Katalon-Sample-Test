f9bce7d4ab3dd79cd923f53ad0c3ce1abf63cf70
haitao.l <lht9977outlook.com>
2022-03-04 11:30:37 +0800

    12

---
+++ Test Suites/Test Suite1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9ffb4e92-b882-44ff-add2-a9af2de672c8</testSuiteGuid>
+   <testCaseLink>
+      <guid>fd43422e-e704-46b5-a0bc-79cec0bdd3a8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Test Suite2.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>4fd06d07-572f-4dd3-9ccc-ae4bc67c0133</testSuiteGuid>
+   <testCaseLink>
+      <guid>b3a9ce32-f145-48cb-8a33-75d31faf3b84</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2eaca4e1-2441-42a9-ae1a-9e46bfa68b81</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case3</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>021b3a0a-4ec3-4f63-803c-21f9804fc34b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case4</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d47f57d6-db7c-450f-a311-87f7d31f00f4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case5</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
